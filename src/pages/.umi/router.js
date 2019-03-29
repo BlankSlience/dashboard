28f7667ca9b683886bf38d8ddb69fbd9fcba93b4
Vicky <1139880650@qq.com>
2019-03-29 18:22:04 +0800

    first commit

---
+++ src/pages/.umi/router.js
@@ -0,0 +1,338 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/_renderRoutes';
+import RendererWrapper0 from '/Users/zhangjing/my-project/src/pages/.umi/LocaleWrapper.jsx'
+import _dvaDynamic from 'dva/dynamic'
+
+let Router = require('dva/router').routerRedux.ConnectedRouter;
+
+let routes = [
+  {
+    "path": "/user",
+    "component": _dvaDynamic({
+  
+  component: () => import(/* webpackChunkName: "layouts__UserLayout" */'../../layouts/UserLayout'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+    "routes": [
+      {
+        "path": "/user",
+        "redirect": "/user/login",
+        "exact": true
+      },
+      {
+        "path": "/user/login",
+        "name": "login",
+        "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__User__models__register.js' */'/Users/zhangjing/my-project/src/pages/User/models/register.js').then(m => { return { namespace: 'register',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__User__Login" */'../User/Login'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+        "exact": true
+      },
+      {
+        "path": "/user/register",
+        "name": "register",
+        "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__User__models__register.js' */'/Users/zhangjing/my-project/src/pages/User/models/register.js').then(m => { return { namespace: 'register',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__User__Register" */'../User/Register'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+        "exact": true
+      },
+      {
+        "path": "/user/register-result",
+        "name": "register.result",
+        "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__User__models__register.js' */'/Users/zhangjing/my-project/src/pages/User/models/register.js').then(m => { return { namespace: 'register',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__User__RegisterResult" */'../User/RegisterResult'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+        "exact": true
+      },
+      {
+        "component": _dvaDynamic({
+  
+  component: () => import(/* webpackChunkName: "p__404" */'../404'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+        "exact": true
+      },
+      {
+        "component": () => React.createElement(require('/Users/zhangjing/my-project/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+      }
+    ]
+  },
+  {
+    "path": "/",
+    "component": _dvaDynamic({
+  
+  component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../../layouts/BasicLayout'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+    "Routes": [require('../Authorized').default],
+    "routes": [
+      {
+        "path": "/",
+        "redirect": "/dashboard/analysis",
+        "authority": [
+          "admin",
+          "user"
+        ],
+        "exact": true
+      },
+      {
+        "path": "/dashboard",
+        "name": "dashboard",
+        "icon": "dashboard",
+        "routes": [
+          {
+            "path": "/dashboard/analysis",
+            "name": "analysis",
+            "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__Dashboard__models__activities.js' */'/Users/zhangjing/my-project/src/pages/Dashboard/models/activities.js').then(m => { return { namespace: 'activities',...m.default}}),
+  import(/* webpackChunkName: 'p__Dashboard__models__chart.js' */'/Users/zhangjing/my-project/src/pages/Dashboard/models/chart.js').then(m => { return { namespace: 'chart',...m.default}}),
+  import(/* webpackChunkName: 'p__Dashboard__models__monitor.js' */'/Users/zhangjing/my-project/src/pages/Dashboard/models/monitor.js').then(m => { return { namespace: 'monitor',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__Dashboard__Analysis" */'../Dashboard/Analysis'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+            "exact": true
+          },
+          {
+            "path": "/dashboard/monitor",
+            "name": "monitor",
+            "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__Dashboard__models__activities.js' */'/Users/zhangjing/my-project/src/pages/Dashboard/models/activities.js').then(m => { return { namespace: 'activities',...m.default}}),
+  import(/* webpackChunkName: 'p__Dashboard__models__chart.js' */'/Users/zhangjing/my-project/src/pages/Dashboard/models/chart.js').then(m => { return { namespace: 'chart',...m.default}}),
+  import(/* webpackChunkName: 'p__Dashboard__models__monitor.js' */'/Users/zhangjing/my-project/src/pages/Dashboard/models/monitor.js').then(m => { return { namespace: 'monitor',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__Dashboard__Monitor" */'../Dashboard/Monitor'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+            "exact": true
+          },
+          {
+            "component": () => React.createElement(require('/Users/zhangjing/my-project/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+          }
+        ]
+      },
+      {
+        "path": "/list",
+        "icon": "table",
+        "name": "list",
+        "routes": [
+          {
+            "path": "/list/table-list",
+            "name": "searchtable",
+            "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__List__models__rule.js' */'/Users/zhangjing/my-project/src/pages/List/models/rule.js').then(m => { return { namespace: 'rule',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__List__TableList" */'../List/TableList'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+            "exact": true
+          },
+          {
+            "path": "/list/basic-list",
+            "name": "basiclist",
+            "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__List__models__rule.js' */'/Users/zhangjing/my-project/src/pages/List/models/rule.js').then(m => { return { namespace: 'rule',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__List__BasicList" */'../List/BasicList'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+            "exact": true
+          },
+          {
+            "component": () => React.createElement(require('/Users/zhangjing/my-project/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+          }
+        ]
+      },
+      {
+        "name": "account",
+        "icon": "user",
+        "path": "/account",
+        "hideInMenu": true,
+        "routes": [
+          {
+            "path": "/account/center",
+            "name": "center",
+            "component": _dvaDynamic({
+  
+  component: () => import(/* webpackChunkName: "p__Account__Center__Center" */'../Account/Center/Center'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+            "routes": [
+              {
+                "path": "/account/center",
+                "redirect": "/account/center/articles",
+                "exact": true
+              },
+              {
+                "path": "/account/center/articles",
+                "component": _dvaDynamic({
+  
+  component: () => import(/* webpackChunkName: "p__Account__Center__Center" */'../Account/Center/Articles'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+                "exact": true
+              },
+              {
+                "path": "/account/center/applications",
+                "component": _dvaDynamic({
+  
+  component: () => import(/* webpackChunkName: "p__Account__Center__Center" */'../Account/Center/Applications'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+                "exact": true
+              },
+              {
+                "path": "/account/center/projects",
+                "component": _dvaDynamic({
+  
+  component: () => import(/* webpackChunkName: "p__Account__Center__Center" */'../Account/Center/Projects'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+                "exact": true
+              },
+              {
+                "component": () => React.createElement(require('/Users/zhangjing/my-project/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+              }
+            ]
+          },
+          {
+            "path": "/account/settings",
+            "name": "settings",
+            "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__Account__Settings__models__geographic.js' */'/Users/zhangjing/my-project/src/pages/Account/Settings/models/geographic.js').then(m => { return { namespace: 'geographic',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__Account__Settings__Info" */'../Account/Settings/Info'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+            "routes": [
+              {
+                "path": "/account/settings",
+                "redirect": "/account/settings/base",
+                "exact": true
+              },
+              {
+                "path": "/account/settings/base",
+                "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__Account__Settings__models__geographic.js' */'/Users/zhangjing/my-project/src/pages/Account/Settings/models/geographic.js').then(m => { return { namespace: 'geographic',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__Account__Settings__Info" */'../Account/Settings/BaseView'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+                "exact": true
+              },
+              {
+                "path": "/account/settings/security",
+                "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__Account__Settings__models__geographic.js' */'/Users/zhangjing/my-project/src/pages/Account/Settings/models/geographic.js').then(m => { return { namespace: 'geographic',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__Account__Settings__Info" */'../Account/Settings/SecurityView'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+                "exact": true
+              },
+              {
+                "path": "/account/settings/binding",
+                "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__Account__Settings__models__geographic.js' */'/Users/zhangjing/my-project/src/pages/Account/Settings/models/geographic.js').then(m => { return { namespace: 'geographic',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__Account__Settings__Info" */'../Account/Settings/BindingView'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+                "exact": true
+              },
+              {
+                "path": "/account/settings/notification",
+                "component": _dvaDynamic({
+  app: window.g_app,
+models: () => [
+  import(/* webpackChunkName: 'p__Account__Settings__models__geographic.js' */'/Users/zhangjing/my-project/src/pages/Account/Settings/models/geographic.js').then(m => { return { namespace: 'geographic',...m.default}})
+],
+  component: () => import(/* webpackChunkName: "p__Account__Settings__Info" */'../Account/Settings/NotificationView'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+                "exact": true
+              },
+              {
+                "component": () => React.createElement(require('/Users/zhangjing/my-project/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+              }
+            ]
+          },
+          {
+            "component": () => React.createElement(require('/Users/zhangjing/my-project/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+          }
+        ]
+      },
+      {
+        "component": _dvaDynamic({
+  
+  component: () => import(/* webpackChunkName: "p__404" */'../404'),
+  LoadingComponent: require('/Users/zhangjing/my-project/src/components/PageLoading/index').default,
+}),
+        "exact": true
+      },
+      {
+        "component": () => React.createElement(require('/Users/zhangjing/my-project/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+      }
+    ]
+  },
+  {
+    "component": () => React.createElement(require('/Users/zhangjing/my-project/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+  }
+];
+window.g_routes = routes;
+window.g_plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+// route change handler
+function routeChangeHandler(location, action) {
+  window.g_plugins.applyForEach('onRouteChange', {
+    initialValue: {
+      routes,
+      location,
+      action,
+    },
+  });
+}
+window.g_history.listen(routeChangeHandler);
+routeChangeHandler(window.g_history.location);
+
+export default function RouterWrapper() {
+  return (
+<RendererWrapper0>
+          <Router history={window.g_history}>
+      { renderRoutes(routes, {}) }
+    </Router>
+        </RendererWrapper0>
+  );
+}
